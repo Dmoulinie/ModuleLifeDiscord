de6554474a725fddf5293465a40041735f44878b
dmoulinie <dan.moulinie@gmail.com>
2022-12-12 17:38:24 +1100

    Remove everyone + name main

--- bot.py
+++ bot.py
@@ -1,41 +1,44 @@
 import os
 import hikari
 import lightbulb
+import miru
+import sys
 from dotenv import load_dotenv
 
-load_dotenv()
-bot = lightbulb.BotApp(
-    os.getenv('token'),
+if __name__ == "__main__":
+
+    load_dotenv()
+    __BOT__ = lightbulb.BotApp(os.getenv('token'),
     intents=hikari.Intents.ALL,
     ) # Token
+    miru.load(__BOT__)
 
-@bot.listen(hikari.StartedEvent)
-async def start_hello(event):
-    print("Started...")
-
-@bot.command()
-@lightbulb.option("text", "Le texte a dire", str, required=True)
-@lightbulb.command("say", "Dit ce que tu veux")
-# @lightbulb.implements(lightbulb.SlashCommand)
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
-async def command_say(ctx : lightbulb.SlashCommand) -> None:
-    await ctx.respond(ctx.options.text)
+    @__BOT__.listen(hikari.StartedEvent) # When the bot is ready
+    async def start_hello(event):
+        print("Started...")
 
 
-
-@bot.command()
-@lightbulb.command("cfq", "Demande ça fait quoi")
-# @lightbulb.implements(lightbulb.SlashCommand)
-@lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
-async def command_cfq(ctx : lightbulb.SlashCommand) -> None:
-    await ctx.respond("ça fait quoi ? @everyone")
+    @__BOT__.command()
+    @lightbulb.option("text", "Le texte a dire", str, required=True)
+    @lightbulb.command("say", "Dit ce que tu veux")
+    # @lightbulb.implements(lightbulb.SlashCommand)
+    @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+    async def command_say(ctx : lightbulb.SlashCommand) -> None:
+        await ctx.respond(ctx.options.text)
 
 
 
+    @__BOT__.command()
+    @lightbulb.command("cfq", "Demande ça fait quoi")
+    # @lightbulb.implements(lightbulb.SlashCommand)
+    @lightbulb.implements(lightbulb.PrefixCommand, lightbulb.SlashCommand)
+    async def command_cfq(ctx : lightbulb.SlashCommand) -> None:
+        await ctx.respond("ça fait quoi ?")
 
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    if event.is_human:
+    @__BOT__.listen(hikari.GuildMessageCreateEvent)
+    async def print_message(event):
+        if not event.is_human:
+            return
         if event.content:
             switcher = {
                     "yo": "ya",
@@ -71,4 +74,5 @@ async def print_message(event):
             response = switcher.get(str(event.content).lower())
             if response:
                 await event.message.respond(response)
-bot.run()
+
+    __BOT__.run()
