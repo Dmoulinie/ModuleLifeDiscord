bebeea4d5272be90cc1d4a64db707db7747aaaec
ldesfontaine <lucas.desfontaine1@gmail.com>
2024-11-16 09:42:57 +1100

    initial commit

--- dist/events/ready.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (bot) => {
-    bot.once('ready', () => {
-        console.log(`Bot connecté en tant que ${bot.user?.tag}`);
-    });
-};
